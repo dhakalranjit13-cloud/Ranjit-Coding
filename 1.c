13779cb1fd136cad620dd90a9909ce93e00d4358
dhakalranjit13-cloud <dhakalranjit13@gmail.com>
2026-01-13 21:19:00 +0545

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int n,a=1;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    do{
+    
+        printf("%d\n",a);
+            a=a+1;
+    }while(a<=n);
+    }
